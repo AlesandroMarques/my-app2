179d2d485126fa5dc9aeb4091e4071c3d3c139c2
AlesandroMarques <alesandro.marques.am@gmail.com>
2020-08-26 12:17:25 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Cloud On Sync</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Simple React Web App
         </p>
       </div>
     );
