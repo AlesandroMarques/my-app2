f0a3d7b3a975df7f04c92f57999c3e2a4040df5e
AlesandroMarques <alesandro.marques.am@gmail.com>
2020-09-04 10:25:54 -0400

    example nav bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-import logo from './cloudOnsync.png';
+import logo from  './cloudOnsync.png';
 import './App.css';
+import Navibar from './Navibar/Navibar';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navibar/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+       
           <h1 className="App-title">Welcome to Cloud On Sync</h1>
         </header>
         <p className="App-intro">
           Simple React Web App
         </p>
-      </div>
-    );
+        <img src={logo}/>
+       
+      </div>);
   }
 }
 
